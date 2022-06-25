1ca18fad5bdb05963cdca1cfdadab3e02401f320
JungHo-K1m <62466374+JungHo-K1m@users.noreply.github.com>
2022-06-25 16:02:45 +0900

    주석처리

--- src/makeEval.java
+++ src/makeEval.java
@@ -3,62 +3,72 @@ import java.util.Stack;
 
 public class makeEval {
     public static double eval(final String str){
-        char[] token = str.toCharArray();
+        char[] token = str.toCharArray();   //문자열을 쪼개자
         String oper = "+-*/";
 
-        Stack<Integer> values = new Stack<Integer>();
-        Stack<Character> operations = new Stack<Character>();
+        Stack<Integer> values = new Stack<Integer>();   //숫자를 넣을 스택
+        Stack<Character> operations = new Stack<Character>();   //연산자를 넣을 스택
 
+        //입력한 문자열 길이만큰 반복
         for(int i=0; i < str.length(); i++){
-            if(token[i] == ' ') continue;
+            if(token[i] == ' ') continue;   //빈칸(스페이스바)는 건너뛰자
 
+            //읽은 문자가 숫자인 경우
             if(Character.isDigit(token[i])){
-                StringBuffer valueBuffer = new StringBuffer();
+                StringBuffer valueBuffer = new StringBuffer();  //숫자를 담아놓을 버퍼 생성
 
+                //i가 토큰의 길이보다 작고 숫자인 경우
                 while(i < token.length && Character.isDigit(token[i]))
-                    valueBuffer.append(token[i++]);
+                    valueBuffer.append(token[i++]); //다음 토큰의 문자를 숫자버퍼에 저장
 
-                values.push(Integer.parseInt(valueBuffer.toString()));
+                values.push(Integer.parseInt(valueBuffer.toString()));  //숫자버퍼의 문자를 10진수로 변졍하여 숫자 스택에 넣자
 
-                i--;
+                i--;    //숫자가 끝나는 지점(연산자 앞)으로 i설정
             }
 
+            //읽은 문자가 시작하는 괄호인 경우
             else if(token[i] == '(')
-                operations.push(token[i]);
+                operations.push(token[i]);  //연산자를 넣는 스택에 넣자
 
+
+            //읽은 문자가 닫는 괄호인 경우
             else if(token[i] == ')'){
-                while (operations.peek() != '(')
-                    values.push(applyOp(operations.pop(), values.pop(), values.pop()));
-                operations.pop();
+                while (operations.peek() != '(')    //스택의 마지막에 들어간 값이 시작 괄호인지 확인
+                    values.push(applyOp(operations.pop(), values.pop(), values.pop())); // 값을 계산하여 숫자 스택에 넣는다.
+                operations.pop();   //연산자 스택의 마지막 문자 반환
             }
 
+            //토큰의 i번째 문자가 연산자인 경우
             else if(oper.indexOf(token[i]) != -1){
+                //연산자 스택이 비어있지 않고, 연산 우선순위가 true인 경우
                 while (!operations.empty() && hasPrecedence(token[i], operations.peek()))
-                    values.push(applyOp(operations.pop(), values.pop(), values.pop()));
-                operations.push(token[i]);
+                    values.push(applyOp(operations.pop(), values.pop(), values.pop())); //값을 계산하여 숫자 스택에 넣는다
+                operations.push(token[i]);  //연산자 스택에 넣는다
             }
         }
+
+        //연산자 스택이 비어있지 않은 경우(마지막 계산)
         while (!operations.empty())
-            values.push(applyOp(operations.pop(), values.pop(), values.pop()));
+            values.push(applyOp(operations.pop(), values.pop(), values.pop())); //값을 계산하여 숫자 스택에 넣는다
 
-
+        //계산 결과 반환
         return values.pop();
     }
 
-    public static boolean hasPrecedence(
-            char op1, char op2)
-    {
+    //우선순위 확인
+    public static boolean hasPrecedence(char op1, char op2){
+        //두번째로 받은 문자(연산자 스택의 마지막 문자)
         if (op2 == '(' || op2 == ')')
             return false;
+        //첫 번째로 받은 문자(토큰의 i번째 문자)
         if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
             return false;
         return true;
     }
 
-    public static int applyOp(char op, int b, int a)
-    {
-        switch (op)
-        {
+    //실제 계산(사칙연산)
+    public static int applyOp(char op, int b, int a){
+        switch (op){
             case '+':
                 return a + b;
             case '-':
@@ -66,7 +76,7 @@ public class makeEval {
             case '*':
                 return a * b;
             case '/':
-                if (b == 0)
+                if (b == 0) //나누는 수가 0인 경우 예외처리
                     throw new
                             UnsupportedOperationException("Cannot divide by zero");
                 return a / b;
@@ -76,7 +86,7 @@ public class makeEval {
 
 
     public static void main(String[] args){
-        System.out.println("입력 : ");
+        System.out.print("입력 : ");
         Scanner scanner = new Scanner(System.in);
 
         String str = scanner.next();
